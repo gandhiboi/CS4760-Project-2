708b2bc147434ef6761e267121270da229d0c0d9
gandhiboi <krijestorac.kenan@gmail.com>
2021-02-21 02:52:36 -0600

    finished setting up shared memory, load numbers into shared memory, etc,

--- master.cpp
+++ master.cpp
@@ -11,6 +11,9 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <fstream>
+#include <signal.h>
+#include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -25,7 +28,11 @@ struct SharedMemory {
         int value;
 };
 
-
+void attachShmem();
+void releaseShmem();
+void deleteShmem();
+void storeShmem(vector<int>, int);
+vector<int> checkNumStatus(vector<int>, int);
 
 void usage();
 
@@ -39,8 +46,10 @@ int main(int argc, char * argv[]) {
 	
 	int userChildren = 20;
 	int userTime = 100;
-	
-	string fileName;
+
+	int vecSize;	
+
+	char * dataFile;
 
 	while((opt = getopt(argc, argv, "hs:t:")) != -1) {
 		switch(opt) {
@@ -68,10 +77,97 @@ int main(int argc, char * argv[]) {
 		}
 	}
 	
-	key = ftok("makefile", 'p');                                                                                    //key generated for shared memory
+	//key genereated for shared memory
+	key = ftok("makefile", 'p');
 
-	//allocates shared memory
-        if((shmId = shmget(key, sizeof(struct SharedMemory), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+	//allocates and attaches shared memory
+	attachShmem();
+
+	if(argv[optind] == NULL) {
+		perror("master.cpp: error: no input file");
+		exit(EXIT_FAILURE);
+	}
+	
+	dataFile = argv[optind];
+
+	FILE *fp = fopen(dataFile, "r");
+	int errVal;
+	vector<int> numberHolder;
+	char *readLine = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	if(fp == NULL) {
+		perror("master.cpp: error: failed to open file");
+		exit(EXIT_FAILURE);
+	}
+	
+	while(read = getline(&readLine, &len, fp) != -1) {
+		int index = atoi(readLine);
+		numberHolder.push_back(index);
+	}
+	fclose(fp);
+	free(readLine);
+
+	vecSize = numberHolder.size();
+/*
+	for(int i = 0; i < vecSize; i++) {
+		shmem[i].value = numberHolder[i];
+		shmem[i].pgid = 0;
+		shmem[i].done = false;
+		shmem[i].ready = true;
+		shmem[i].depth = -1;
+	}
+
+
+	for(int i = 0; i < vecSize; i++) {
+		cout << shmem[i].value << endl;
+	}
+*/
+
+
+	int index= numberHolder.size();
+        int target = 0;
+
+        while (index >>= 1) {
+                ++target;
+        }
+
+	cout << target << endl << endl << endl;
+
+        if(pow(2,target) < numberHolder.size()) {
+		
+		//target++;
+
+		cout << "AM IN THIS THING?" << endl << endl;
+		
+                //target++;
+                do {
+			cout << "target var: " << target << endl << endl;
+                        numberHolder.push_back(0);
+			cout << "size of number holder: " << numberHolder.size() << endl << endl;
+             }
+                while(pow(2,target) < numberHolder.size());
+        }
+
+
+
+	
+
+
+	for(int i = 0; i < vecSize; i++)
+		cout << numberHolder.at(i) << endl;
+
+	//storeShmem(numberHolder, vecSize);
+
+
+	return EXIT_SUCCESS;
+
+}
+
+void attachShmem() {
+	
+	if((shmId = shmget(key, sizeof(struct SharedMemory), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
                 perror("master.cpp: error: failed to allocate shared memory");
                 exit(EXIT_FAILURE);
         }
@@ -79,19 +175,64 @@ int main(int argc, char * argv[]) {
                 shmem = (struct SharedMemory*)shmat(shmId, NULL, 0);
         }
 
+}
 
-	if(argv[optind] == NULL) {
-		perror("master.cpp: error: no input file");
-		exit(EXIT_FAILURE);
+void releaseShmem() {
+
+	if(shmem != NULL) {
+		if(shmdt(shmem)) {
+			perror("master.cpp: error: failed to release shared memory");
+			exit(EXIT_FAILURE);
+		}
 	}
-	else {
-		fileName = argv[optind];
+}
+
+void deleteShmem() {
+	
+	if(shmId > 0) {
+		if(shmctl(shmId, IPC_RMID, NULL) < 0) {
+			perror("master.cpp: error: failed to delete shared memory");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+void storeShmem(vector<int> vect1, int size1) {
+
+	for(int i = 0; i < size1; i++) {
+                shmem[i].value = vect1[i];
+                shmem[i].pgid = 0;
+                shmem[i].done = false;
+                shmem[i].ready = true;
+                shmem[i].depth = -1;
+        }
+
+
+        for(int i = 0; i < size1; i++) {
+                cout << shmem[i].value << endl;
+        }
+
+}
+
+vector<int> checkNumStatus(vector<int> vect2, int size2) {
+
+	int indexer = vect2.size();
+	int target = 0;
+
+	while (indexer >>= 1) {
+		++target;
 	}
 
-	
+	if(pow(2,target) < vect2.size()) {
+		target++;
+		do {
+			vect2.push_back(0);
+		}
+		while(pow(2,target) < vect2.size());
+	}
 	
 
-	return EXIT_SUCCESS;
+	return vect2;
 
 }
 
