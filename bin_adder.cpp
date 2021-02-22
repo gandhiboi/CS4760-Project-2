ee9b45455b4a469308eb13b21f554c2a9df5d315
gandhiboi <krijestorac.kenan@gmail.com>
2021-02-22 03:28:15 -0600

    made sure shared memory is working

--- bin_adder.cpp
+++ bin_adder.cpp
@@ -1,18 +1,66 @@
 #include <iostream>
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
-#include <string.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <fstream>
+#include <signal.h>
+#include <vector>
+#include <math.h>
 
 using namespace std;
 
+
+const int maxChildProcess = 20;
+const int maxTimeSeconds = 100;
+
+struct SharedMemory {
+        bool ready;
+        bool done;
+        pid_t pgid;
+        int depth;
+        int value;
+	int vecSize;
+};
+
+key_t key;
+int shmId;
+struct SharedMemory* shmem;
+
+void attachShmem();
+
 int main(int argc, char * argv[]) {
 
+	key = ftok("makefile", 'p');
 
-	cout << "Hello World: bin_adder.cpp" << endl;
+	attachShmem();
+
+	cout << "BIN_ADDER:\t\t";
+
+	for(int i = 0; i < shmem->vecSize; i++)
+		cout << "" << shmem[i].value << " ";
+	
+	cout << endl;
 
 
-	return EXIT_SUCCESS;
+	return 0;
 
+}
+
+void attachShmem() {
+
+	if((shmId = shmget(key, sizeof(struct SharedMemory), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+                perror("master.cpp: error: failed to allocate shared memory");
+                exit(EXIT_FAILURE);
+        }
+        else {
+                shmem = (struct SharedMemory*)shmat(shmId, NULL, 0);
+        }
 }
